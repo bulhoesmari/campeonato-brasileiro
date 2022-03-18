56aad96fa4ec96b2eaafafd6ebbea9cfa4932069
Raul Sousa <raulpedrouag@gmail.com>
2022-03-18 17:36:46 -0300

    ajuste arquivo AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Modificação de teste de merge</h1>
+    <h2>Vai dar conflito</h2>
+    <p>paragrafo de teste</p>
   </div>
 </template>
